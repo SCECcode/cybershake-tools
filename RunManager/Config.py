8c286d83879d8730945de02ef3831064f5700659
Patrick Small <patrices@usc.edu>
2009-03-20 19:41:14 +0000

    Added new run state 'Verify Error' to be used when a run verification fails

--- RunManager/Config.py
+++ RunManager/Config.py
@@ -23,7 +23,7 @@ DELETED_STATE = "Deleted"
 SGT_STATES = ["SGT Started", "SGT Generated", "SGT Error",]
 PP_STATES = ["PP Started", "Curves Generated", "PP Error",]
 ACTIVE_STATES = ["Initial", "SGT Started", "SGT Error", "SGT Generated", \
-                     "PP Started", "PP Error", "Curves Generated",]
+                     "PP Started", "PP Error", "Curves Generated", "Verify Error",]
 
 # Dictionary expressing the state-transition-diagram
 STATUS_STD = {"Initial": ["Initial", "SGT Started", "Deleted",], \
@@ -32,7 +32,8 @@ STATUS_STD = {"Initial": ["Initial", "SGT Started", "Deleted",], \
               "SGT Generated":["SGT Generated", "PP Started", "Deleted",], \
               "PP Started":["PP Started", "Curves Generated", "PP Error", "Deleted",], \
               "PP Error":["SGT Generated", "PP Started", "PP Error", "Deleted",], \
-              "Curves Generated":["Curves Generated", "Verified", "Deleted",], \
+              "Curves Generated":["Curves Generated", "Verified", "Verify Error", "Deleted",], \
+              "Verify Error":["Verify Error", "Verified", "Deleted"], \
               "Verified":["Verified",], \
               "Deleted":["Deleted", ]}
 
