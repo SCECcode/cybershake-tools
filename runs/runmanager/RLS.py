557f53412691c438ac905ba16c84699ec99dba89
Scott Callaghan <scottcal@usc.edu>
2009-03-23 22:26:48 +0000

    Changed RLS.py to allow the user to specify a particular RLS host

--- runs/runmanager/RLS.py
+++ runs/runmanager/RLS.py
@@ -6,16 +6,22 @@ import os
 import sys
 import subprocess
 
+
 # Constants
-RLS_HOST = "rls://shock.usc.edu"
+RLS_HOST = "shock.usc.edu"
 
 
 # Globals
 
 
 class RLS:
+    rls_host = None
 
-    def __init__(self):
+    def __init__(self, rls_host = None):
+        if (rls_host != None):
+            self.rls_host = "rls://%s" % (rls_host)
+        else:
+            self.rls_host = "rls://%s" % (RLS_HOST)
         return
 
     def __runCommand(self, cmd):
@@ -38,7 +44,7 @@ class RLS:
 
     def getPFNs(self, lfn):
 
-        cmd = ['globus-rls-cli',  'query',  'lrc', 'lfn',  lfn, RLS_HOST,]
+        cmd = ['globus-rls-cli',  'query',  'lrc', 'lfn',  lfn, self.rls_host,]
         output = self.__runCommand(cmd)
         if (output == None):
             return None
@@ -55,7 +61,7 @@ class RLS:
 
     def renamePFN(self, old_pfn, new_pfn):
 
-        cmd = ['globus-rls-cli',  'rename',  'pfn', old_pfn, new_pfn, RLS_HOST,]
+        cmd = ['globus-rls-cli',  'rename',  'pfn', old_pfn, new_pfn, self.rls_host,]
         output = self.__runCommand(cmd)
         if (output == None):
             return 1
@@ -65,7 +71,7 @@ class RLS:
 
     def getPools(self, pfn):
 
-        cmd = ['globus-rls-cli', 'attribute', 'query', pfn, 'pool', 'pfn', RLS_HOST,]
+        cmd = ['globus-rls-cli', 'attribute', 'query', pfn, 'pool', 'pfn', self.rls_host,]
         output = self.__runCommand(cmd)
         if (output == None):
             return None
@@ -81,7 +87,7 @@ class RLS:
 
 
     def addPool(self, pfn, pool):
-        cmd = ['globus-rls-cli',  'attribute',  'add', pfn, 'pool', 'pfn', 'string', pool, RLS_HOST,]
+        cmd = ['globus-rls-cli',  'attribute',  'add', pfn, 'pool', 'pfn', 'string', pool, self.rls_host,]
         output = self.__runCommand(cmd)
         if (output == None):
             return 1
@@ -91,13 +97,13 @@ class RLS:
 
     def createLFN(self, lfn, pfn, pool=None):
 
-        cmd = ['globus-rls-cli', 'create', lfn, pfn, RLS_HOST,]
+        cmd = ['globus-rls-cli', 'create', lfn, pfn, self.rls_host,]
         output = self.__runCommand(cmd)
         if (output == None):
             return 1
 
         if (pool != None):
-            cmd = ['globus-rls-cli', 'attribute', 'add', pfn, 'pool', 'pfn', 'string', pool, RLS_HOST,]
+            cmd = ['globus-rls-cli', 'attribute', 'add', pfn, 'pool', 'pfn', 'string', pool, self.rls_host,]
             output = self.__runCommand(cmd)
             if (output == None):
                 return 1
@@ -107,7 +113,7 @@ class RLS:
 
     def delete(self, lfn, pfn):
 
-        cmd = ['globus-rls-cli',  'delete',  lfn, pfn, RLS_HOST,]
+        cmd = ['globus-rls-cli',  'delete',  lfn, pfn, self.rls_host,]
         output = self.__runCommand(cmd)
         if (output == None):
             return 1
