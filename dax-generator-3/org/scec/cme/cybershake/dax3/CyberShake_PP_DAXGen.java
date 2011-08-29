3d9208d049f7d0fc4f06d21e4a42ea06bf6b4d9c
Scott Callaghan <scottcal@usc.edu>
2011-08-29 19:15:04 +0000

    Removed double insert of final notify job

--- dax-generator-3/org/scec/cme/cybershake/dax3/CyberShake_PP_DAXGen.java
+++ dax-generator-3/org/scec/cme/cybershake/dax3/CyberShake_PP_DAXGen.java
@@ -457,7 +457,6 @@ public class CyberShake_PP_DAXGen {
     	    
     	    // Create update run state job
     	    Job updateJob = addUpdate(postDax, riq.getRunID(), "PP_START", "PP_END");
-    	    postDax.addJob(updateJob);
     	    
           	return postDax;
     	} catch (Exception ex) {
