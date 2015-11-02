9a63488b82dc40c3ca02c333979cee5aa86c3388
Scott Callaghan <scottcal@usc.edu>
2015-11-02 23:59:14 +0000

    Changed memory calculation for srf2stoch_lite

--- dax-generator-3/org/scec/cme/cybershake/dax3/CyberShake_Sub_Stoch_DAXGen.java
+++ dax-generator-3/org/scec/cme/cybershake/dax3/CyberShake_Sub_Stoch_DAXGen.java
@@ -311,7 +311,7 @@ public class CyberShake_Sub_Stoch_DAXGen {
 
 			job.addProfile("pegasus", "pmc_priority", "" + numPoints);
 		
-			//Memory usage is the size of the SRF + the size of the output, basically.
+			//Memory usage is the size of the SRF, the size of the output, and the size of some mysterious arrays in srf2stoch.
 			double srfMem = 14.8 * Math.log10(numPoints) * Math.pow(numPoints, 1.14) / (1024.0*1024.0);
 			//Largest RV at dt=0.1 is 75 mb, so cap if larger
 			srfMem = Math.min(srfMem, 80.0);
@@ -320,20 +320,25 @@ public class CyberShake_Sub_Stoch_DAXGen {
 			double outputMem = sParams.getTlen()/DT * 2.0 * 4.0 / (1024.0 * 1024.0);
 			//slipfile memory:  3 x NP x NQ x LV x sizeof(float)
 			double slipfileMem = 3.0*100.0*600.0*1000.0*4.0 / (1024.0*1024.0);
-			//Determine memory usage for srf2stoch buffers
+//			//Determine memory usage for srf2stoch buffers
+//			int nstk = numCols;
+//			int ndip = numRows;
+//			int nx = (int)(nstk*0.2/2.0+0.5);
+//			int ny = (int)(ndip*0.2/2.0+0.5);
+//			int nxdiv = 1;
+//			while ((nstk*nxdiv)%nx!=0) {
+//				nxdiv++;
+//			}
+//			int nydiv = 1;
+//			while ((ndip*nydiv)%ny!=0) {
+//				nydiv++;
+//			}
+//			double srf2stochBuffers = 3.0*nxdiv*nydiv*nstk*ndip*4.0/(1024.0*1024.0);
+//			int memUsage = (int)(Math.ceil(1.1*(outputMem + srfMem + slipfileMem+srf2stochBuffers)));
+			//srf2stoch_lite: 
 			int nstk = numCols;
 			int ndip = numRows;
-			int nx = (int)(nstk*0.2/2.0+0.5);
-			int ny = (int)(ndip*0.2/2.0+0.5);
-			int nxdiv = 1;
-			while ((nstk*nxdiv)%nx!=0) {
-				nxdiv++;
-			}
-			int nydiv = 1;
-			while ((ndip*nydiv)%ny!=0) {
-				nydiv++;
-			}
-			double srf2stochBuffers = 3.0*nxdiv*nydiv*nstk*ndip*4.0/(1024.0*1024.0);
+			double srf2stochBuffers = (4.0*nstk*ndip*4.0)/(1024.0*1024.0);
 			int memUsage = (int)(Math.ceil(1.1*(outputMem + srfMem + slipfileMem+srf2stochBuffers)));
 			
 			job.addProfile("pegasus", "pmc_request_memory", "" + memUsage);
