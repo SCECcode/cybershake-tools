11a246dc4daebe4c0d7d2969de48655768adaefa
Scott Callaghan <scottcal@usc.edu>
2021-10-22 16:31:09 -0700

    Updated to getDeclaredConstructor()

--- dax-generator-3/org/scec/cme/cybershake/dax3/DBConnect.java
+++ dax-generator-3/org/scec/cme/cybershake/dax3/DBConnect.java
@@ -196,7 +196,7 @@ public class DBConnect
     //and the contents of the stack
     try
     {
-      Class.forName(drivers).getConstructor(null).newInstance();
+      Class.forName(drivers).getDeclaredConstructor(null).newInstance();
       //Class.forName(drivers).newInstance();
     }
     catch (Exception E)
