5d3c5b3b420d3b330d256a2b833ca42133115965
Scott Callaghan <scottcal@usc.edu>
2024-08-27 11:34:16 -0700

    Updated to use 20x40x1 GPUs (100 nodes) for Study 24.8.

--- dax-generator-3/org/scec/cme/cybershake/dax3/CyberShake_AWP_SGT_DAXGen.java
+++ dax-generator-3/org/scec/cme/cybershake/dax3/CyberShake_AWP_SGT_DAXGen.java
@@ -563,18 +563,19 @@ public class CyberShake_AWP_SGT_DAXGen {
 					procDims[1] = 10;
 					procDims[2] = 1;
 				} else {
+					//Study 24.8: 20x40x1
 					//Study 18.8: change to 40x30x1
 					//Go back to 20x20x1 for Summit testing
 					if (dims[0] % 40 != 0) {
 						System.err.println("One of the volume dimensions is " + dims[0] + " which is not divisible by 40.  Aborting.");
 						System.exit(3);
 					}
-					if (dims[1] % 40 != 0) {
-						System.err.println("One of the volume dimensions is " + dims[1] + " which is not divisible by 40.  Aborting.");
+					if (dims[1] % 80 != 0) {
+						System.err.println("One of the volume dimensions is " + dims[1] + " which is not divisible by 80.  Aborting.");
 						System.exit(3);
 					}
 					procDims[0] = 20;
-					procDims[1] = 20;
+					procDims[1] = 40;
 					procDims[2] = 1;
 				}
 				
